6f3ad8cf681d75529ddeb68d9b62fda1a7a9a04b
yuya0628 <sonoyuki13@gmail.com>
2021-09-18 22:49:55 +0900

    インデント等修正

--- Rock-paper-scissors.rb
+++ Rock-paper-scissors.rb
@@ -1,109 +1,104 @@
-puts "何本勝負？(press 1 or 3 or 5)"
+puts '何本勝負？(press 1 or 3 or 5)'
 n = gets.to_i
-if n == 1||n == 3||n == 5
+if [1, 3, 5].include?(n)
   puts "#{n}本勝負を選びました"
 else
-  puts "1,3,5以外は入力できません"
-  exit #処理終了
-end  
+  puts '1, 3, 5以外は入力できません'
+  exit # 処理終了
+end
 
-  v = 0  # 勝の初期値(forの外に書く)
-  d = 0  # 敗の初期値（forの外に書く）
+v = 0  # 勝の初期値(forの外に書く)
+d = 0  # 敗の初期値（forの外に書く）
 
 (1..n).each do |i|
   puts "#{i}本目"
-  puts "じゃんけん…(press g or c or p)"
-  x = gets.to_s.chomp  #getsの改行コードを消すchompメソッド
-  
-  if x == "g"
-    puts "あなた…グー"
-  elsif x == "c"
-    puts "あなた…チョキ"
-  elsif x == "p"
-    puts "あなた…パー"
-  else             #g,c,p以外を選んだ場合
-    loop{
-    puts "g,c,pを選んでください"
-    x = gets.to_s.chomp
-    if x == "g"
-      puts "あなた…グー"
-      break
-    elsif x == "c"
-      puts "あなた…チョキ"
-      break
-    elsif x == "p"
-      puts "あなた…パー"
-      break
-    end  
-    }
-  end 
+  puts 'じゃんけん…(press g or c or p)'
+  x = gets.to_s.chomp # getsの改行コードを消すchompメソッド
 
-  y = rand(1..3)
-    if y == 1
-      puts "CPU…グー"
-    elsif y == 2
-      puts "CPU…チョキ"
-    elsif y == 3
-      puts "CPU…パー"
-    end 
+  if x == 'g'
+    puts 'あなた…グー'
+  elsif x == 'c'
+    puts 'あなた…チョキ'
+  elsif x == 'p'
+    puts 'あなた…パー'
+  else # g, c, p以外を選んだ場合
+    loop do
+      puts 'g, c, pを選んでください'
+      x = gets.to_s.chomp
+      if x == 'g'
+        puts 'あなた…グー'
+        break
+      elsif x == 'c'
+        puts 'あなた…チョキ'
+        break
+      elsif x == 'p'
+        puts 'あなた…パー'
+        break
+      end
+    end
+  end
 
-    
+  y = %w[g c p]
+  t = y[rand(3)]
+  if t == 'g'
+    puts 'CPU…グー'
+  elsif t == 'c'
+    puts 'CPU…チョキ'
+  elsif t == 'p'
+    puts 'CPU…パー'
+  end
 
-loop do # 決着が着くまで繰り返す
+  loop do # 決着が着くまで繰り返す
+    if t == 'c' && x == 'g' || t == 'p' && x == 'c' || t == 'g' && x == 'p'
+      puts '勝ち'
+      v += 1 # v + 1 = v
+      puts "#{v}勝#{d}敗"
+      break # 処理終わり（loop文）
 
- if y == 2 && x == "g" || y == 3 && x == "c" || y == 1 && x == "p"
-    puts "勝ち"
-    v += 1  # v + 1 = v
-    puts "#{v}勝#{d}敗"
-    break  #処理終わり（loop文）
+    elsif t == 'p' && x == 'g' || t == 'g' && x == 'c' || t == 'c' && x == 'p'
+      puts '負け'
+      d += 1 # d + 1 = d
+      puts "#{v}勝#{d}敗"
+      break # 処理終わり(loop文）
 
- elsif y == 3 && x == "g" || y == 1 && x == "c" || y == 2 && x == "p"
-    puts "負け"
-    d += 1  # d + 1 = d
-    puts "#{v}勝#{d}敗"
-    break  #処理終わり(loop文）
-
- else 
-    puts "あいこで...(press g or c or p)" 
-    x = gets.to_s.chomp  
-        if x == "g"
-          puts "あなた…グー"
-        elsif x == "c"
-          puts "あなた…チョキ"
-        elsif x == "p"
-          puts "あなた…パー"
-        else             #g,c,p以外を選んだ場合
-          loop{
-          puts "g,c,pを選んでください"
+    else
+      puts 'あいこで...(press g or c or p)'
+      x = gets.to_s.chomp
+      if x == 'g'
+        puts 'あなた…グー'
+      elsif x == 'c'
+        puts 'あなた…チョキ'
+      elsif x == 'p'
+        puts 'あなた…パー'
+      else # g,c,p以外を選んだ場合
+        loop do
+          puts 'g, c, pを選んでください'
           x = gets.to_s.chomp
-          if x == "g"
-            puts "あなた…グー"
+          if x == 'g'
+            puts 'あなた…グー'
             break
-          elsif x == "c"
-            puts "あなた…チョキ"
+          elsif x == 'c'
+            puts 'あなた…チョキ'
             break
-          elsif x == "p"
-            puts "あなた…パー"
+          elsif x == 'p'
+            puts 'あなた…パー'
             break
-          end  
-          }
-        end 
+          end
+        end
+      end
 
-    y = rand(1..3)
-          if y == 1
-            puts "CPU…グー"
-          elsif y == 2
-            puts "CPU…チョキ"
-          elsif y == 3
-            puts "CPU…パー"
-          end 
-      
-          
-       
-  end 
+      y = %w[g c p]
+      t = y[rand(3)]
+      if t == 'g'
+        puts 'CPU…グー'
+      elsif t == 'c'
+        puts 'CPU…チョキ'
+      elsif t == 'p'
+        puts 'CPU…パー'
+      end
 
-end # 決着が着くまで繰り返す
-
+    end
+  end # 決着が着くまで繰り返す
 end # each文終わり
 
 if v > d
